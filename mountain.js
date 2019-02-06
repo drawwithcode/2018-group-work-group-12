dd27d977a75d4af2d18c49cfd16ad8e360fba52a
Bao YiFang <byf.aisling@gmail.com>
2019-02-06 08:08:26 +0100

    modification

--- mountain.js
+++ mountain.js
@@ -1,80 +1,3 @@
-// var imgs = [];
-// var imgIndex = -1;
-// var img;
-// var paint;
-// var subStep = 800;
-// var z = 0;
-// var isStop = false;
-//
-// function preload() {
-//   imgs[0] = loadImage("pic1.png");
-//   //imgs[1] = loadImage("test2.png");
-//   //imgs[2] = loadImage("test3.png");
-// }
-//
-// function setup() {
-//   //if(windowWidth < 600)
-//     //createCanvas(windowWidth, windowWidth);
-//   //else
-//     createCanvas(600, 600);
-//   img = createImage(width, height);
-//   nextImage();
-//   paint = new Paint(createVector(width/2, height/2));
-//   background(255, 255, 255);
-//   colorMode(RGB, 255, 255, 255, 255);
-// }
-//
-// function draw() {
-//   //console.log(frameRate());
-//   if (!isStop) {
-//     for (var i = 0 ; i < subStep ; i++) {
-//       paint.update();
-//       paint.show();
-//       z+= 0.01;
-//     }
-//   }
-//   //background(255);
-//   //image(img, 0, 0, width, height);
-// }
-//
-// function fget(i, j) {
-//   var index = j * img.width + i;
-//   index *= 4;
-//   return color(img.pixels[index], img.pixels[index+1], img.pixels[index+2], img.pixels[index+3]);
-// }
-//
-// function fset(i, j, c) {
-//   var index = j * img.width + i;
-//   index *= 4;
-//   img.pixels[index] = red(c);
-//   img.pixels[index+1] = green(c);
-//   img.pixels[index+2] = blue(c);
-//   img.pixels[index+3] = alpha(c);
-// }
-//
-// function keyPressed() {
-//   console.log(key);
-//   if (key === 's' || key === 'S') {
-//     isStop = !isStop;
-//   }
-// }
-// function mouseClicked() {
-//   nextImage();
-// }
-// function touchStarted() {
-//   nextImage();
-// }
-//
-// function nextImage() {
-//   if (!img) return;
-//   imgIndex = (++imgIndex) % imgs.length;
-//   var targetImg = imgs[imgIndex];
-//   img.copy(targetImg, 0, 0, targetImg.width, targetImg.height, 0, 0, img.width, img.height);
-//   //img.resize(width, height);
-//   img.loadPixels();
-//   clear();
-// }
-
 var d;
 var g;
 var x = 90.0;
@@ -101,11 +24,11 @@ function draw() {
   fill(102, 117, g, 10);
   stroke(173, 193, 155, 10);
   angle = angle - 0.01;
-  translate(x, height / 1.2 - d); //set the y coordinate of the circle
+  translate(x, height / 1.2 - d);
   rotate(angle);
-  triangle(-50, -50, -50, -50, 30, 30); //outer white
-  triangle(-50, -50, -50, -50, 60, d); //inner white
-  triangle(-50, -0, -20, -20, 90, 20); //black
+  triangle(-50, -50, -50, -50, 30, 30);
+  triangle(-50, -50, -50, -50, 60, d);
+  triangle(-50, -0, -20, -20, 90, 20);
 
   x = x + speed;
   if (x > width + radius) {
@@ -113,14 +36,10 @@ function draw() {
   }
 }
 
-// this function fires after the mouse has been
-// clicked anywhere
 function mouseClicked() {
   d = d + 10;
 }
 
-// this function fires after the mouse has been
-// clicked on canvas
 function changeGray() {
   g = random(150, 155);
 }

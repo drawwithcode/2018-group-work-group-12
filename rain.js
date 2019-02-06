51aab9b4f8b1165af55fbcfe994be2b3b3e2193e
lemonxixixi <43404481+lemonxixixi@users.noreply.github.com>
2019-02-06 02:54:22 +0100

    Update rain.js

--- rain.js
+++ rain.js
@@ -1,38 +1,51 @@
-var xstart, xnoise, ystart, ynoise;
+var d = [];
 
 function setup() {
-  createCanvas(600, 600);
-  xstart = random(1);
-  ystart = random(1);
-	frameRate(60);
+  createCanvas(windowWidth, windowHeight);
+
+  for (var i = 0; i < 350; i=i+2) {
+    d.push(new Drop(random(0, windowWidth), random(0, windowHeight), random(2, 4)));
+  }
 }
 
-
 function draw() {
-  background(color("#39A0ED"));
-  xstart += 0.1;
-  ystart += 0.1;
-  xnoise = xstart;
-  ynoise = ystart;
-
-  for (var y = 0; y <= height; y += 7) {
-  	ynoise += 0.1;
-  	xnoise = xstart;
-  		for (var x = 0; x <= width; x += 7) {
-  		xnoise += 0.1;
-  		drawPoint(x, y, noise(xnoise, ynoise));
- 			}
- 		}
-	}
-function drawPoint(x, y, noiseFactor) {
-  push();
-  translate(x, y);
-  rotate(noiseFactor * radians(560));
-  var edgeSize = noiseFactor*45;
-  var grey = 170 + (noiseFactor*150);
-  var alph = 150 + (noiseFactor*120);
   noStroke();
-  fill(grey, alph);
-  ellipse(0, 0, edgeSize,edgeSize/2);
-  pop();
+  fill(0, 20);
+  rect(0, 0, width, height);
+  for (var i = 0; i < d.length; i++) {
+    d[i].displ();
+  }
+}
+
+function Drop(x, y, sp) {
+  var x1 = x;
+  var y1 = y;
+  var x2;
+  var y2;
+  var s = sp;
+  var vel;
+
+  this.displ = function() {
+    var mx = 15;
+    if (mx <= 0) {
+        mx = 0.05;
+    }
+    y1 = y1 + s * mx;
+    x2 = x1;
+    y2 = y1 + 50;
+
+    stroke(200);
+    line(x1, y1, x2, y2);
+
+    if (y1 >= windowHeight - 100) {
+      noFill();
+      //stroke(200);
+      noStroke();
+      fill(0, 25,255);
+      ellipse(x1, windowHeight - random(5, 45), random(20, 30), random(1, 4));
+      x1 = random(0, windowWidth);
+      y1 = -120;
+    }
+  }
+
 }
